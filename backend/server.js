f0e58ed4a15aac562296b9ca2b82a6d02952abf2
Cermait <markbodonnell@gmail.com>
2024-12-24 08:09:44 +0000

    .

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 // Catch-all route to serve index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'app.js'));
 });
 
 passport.serializeUser((user, done) => {
