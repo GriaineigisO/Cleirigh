e4111aa12872a877f7dd35dbb45cfe6779d18e09
Cermait <markbodonnell@gmail.com>
2025-01-16 05:18:33 +0000

    .

--- backend/api/get-all-trees.js
+++ backend/api/get-all-trees.js
@@ -55,17 +55,12 @@ export default async function handler(req, res) {
     const treeName = allTrees.map(tree => tree.tree_name);
     const treeID = allTrees.map(tree => tree.tree_id);
 
-    console.log(treeName)
-    console.log(treeID)
-
     // Check if trees exist and return accordingly
     if (allTrees && allTrees.length > 0) {
       // If there are multiple trees for this user
       res.json({
-        trees: allTrees.map((tree) => ({
           treeName: treeName,
-          treeId: treeID,
-        })),
+          treeId: treeID
       });
     } else {
         console.log("fail")
