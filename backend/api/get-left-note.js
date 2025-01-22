0ac6111587df24ed617025df0f2d107ccb31e7d1
Cermait <markbodonnell@gmail.com>
2025-01-22 22:12:47 +0000

    .

--- backend/api/get-left-note.js
+++ backend/api/get-left-note.js
@@ -34,10 +34,7 @@ export default async function handler(req, res) {
         .eq('id', userId)
         .single(); 
   
-      if (currentTreeError) {
-        throw new Error(currentTreeError.message);
-      }
-  
+        console.log(currentTreeData.current_tree_id)
       const currentTree = currentTreeData.current_tree_id;
 
       const {data: getCurrentPage, error: currentPageError } = await supabase
@@ -46,6 +43,8 @@ export default async function handler(req, res) {
         .eq('id', userId)
 
   console.log(getCurrentPage)
+  console.log(currentTree)
+  
       const currentPage = getCurrentPage[0].current_page;
 
       
