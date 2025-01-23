f40746a087f51914fb107926f24de9b017671d1a
Cermait <markbodonnell@gmail.com>
2025-01-23 17:12:59 +0000

    added topics.js

--- frontend/src/Pages/topics.js
+++ frontend/src/Pages/topics.js
@@ -5,14 +5,47 @@ import { Link } from "react-router-dom";
 import LeftSidebar from "../Components/leftSidebar.js";
 
 const Topics = () => {
+  const [topics, setTopics] = useState([]);
+
+  const getAllTopics = async () => {
+    const userId = localStorage.getItem("userId");
+    const response = await fetch(
+      "https://cleirigh-backend.vercel.app/get-all-topics",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId }),
+      }
+    );
+    const data = await response.json();
+    setTopics(data);
+    console.log(topics)
+  };
+
+  useEffect(() => {
+    getAllTopics();
+  }, []);
+
   return (
     <div className="row">
       <LeftSidebar />
 
       <div className="col-lg centre-col">
-
         <h1>Topics</h1>
 
+        <button>Create New Topic</button>
+
+        <div>
+          {topics.map((topic, index) => (
+            <div>
+              <p>{topics[index].topicName}</p>
+              <button>Open</button>
+              <button>Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
