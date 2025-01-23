eff930722a3c9aabb09221d59934431fd452e6ac
Cermait <markbodonnell@gmail.com>
2025-01-23 16:22:44 +0000

    .

--- frontend/src/Pages/topics.js
+++ frontend/src/Pages/topics.js
@@ -5,11 +5,17 @@ import { Link } from "react-router-dom";
 import LeftSidebar from "../Components/leftSidebar.js";
 
 const Topics = () => {
+  return (
+    <div className="row">
+      <LeftSidebar />
 
-    return (
+      <div className="col-lg centre-col">
+
         <h1>Topics</h1>
-    )
 
-}
+      </div>
+    </div>
+  );
+};
 
-export default Topics
+export default Topics;
