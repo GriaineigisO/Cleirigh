1625ce5dc2c423b92305eba86517b3e05c0d54d8
Cermait <markbodonnell@gmail.com>
2025-01-26 04:44:31 +0000

    .

--- frontend/src/Pages/topic.js
+++ frontend/src/Pages/topic.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { capitaliseFirstLetter, convertNumToRelation } from "../library.js";
 import "../style.css";
 import { Link } from "react-router-dom";
+import ReactQuill from "react-quill";
 
 
 const Topic = () => {
