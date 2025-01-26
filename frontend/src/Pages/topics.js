b1fd6729df64675fedfc19fe741814c1f4a202ff
Cermait <markbodonnell@gmail.com>
2025-01-26 04:30:22 +0000

    .

--- frontend/src/Pages/topics.js
+++ frontend/src/Pages/topics.js
@@ -102,11 +102,7 @@ const Topics = () => {
         <div>
           {topicNames.map((topic, index) => (
             <div style={{ display: "flex", flexDirection: "row" }}>
-              <h5><Link to={`topic${topicLinks[index]}`}>{topicNames[index]}</Link></h5>
-              {/* <button onClick={() => handleOpenTopic(topicLinks[index])}>
-                Open
-              </button> */}
-              <button>Delete</button>
+              <h5 className="span-link" onClick={() => handleOpenTopic(topicLinks[index])}>{topicNames[index]}</h5>
             </div>
           ))}
         </div>
