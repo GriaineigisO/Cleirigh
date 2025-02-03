eee8eb3061594b4401a3e186035826b868ffb9d4
Cermait <markbodonnell@gmail.com>
2025-02-03 09:04:13 +0000

    .

--- backend/api/save-repeat-ancestor.js
+++ backend/api/save-repeat-ancestor.js
@@ -90,11 +90,12 @@ export default async function handler(req, res) {
             .from(`tree_${currentTree}`)
             .select("*")
             .eq("ancestor_id", person.mother_id)
+        
         let mother = "";
-        if (getMother[0]) {
-          let mother = getMother[0];
+        if (getMother[0] !== null) {
+          mother = getMother[0];
         }
-
+        
         //finds grandparents
         let pgrandfather = "";
         let pgrandmother = "";
