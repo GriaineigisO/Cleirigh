49373084f6ce569541a79c85fec3e1fb59ad990a
Cermait <markbodonnell@gmail.com>
2025-03-05 18:22:49 +0000

    .

--- frontend/src/Pages/migrationMap.js
+++ frontend/src/Pages/migrationMap.js
@@ -150,7 +150,7 @@ const FamilyMigrationMap = () => {
       />
       <div style={{ marginTop: "10px" }}>
         {progress.total > 0 && (
-          <p style={{textAlign:"center"}}>{progress.current} of {progress.total} lines added. {progress.current/progress.total*100}% Complete</p>
+          <p style={{textAlign:"center"}}>{progress.current} of {progress.total} lines added. {(progress.current/progress.total*100).toFixed(2)}% Complete</p>
         )}
       </div>
     </div>
