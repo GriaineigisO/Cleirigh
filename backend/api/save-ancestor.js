a7556de10bb8ec2843adc752d868e8d8be99c7be
Cermait <markbodonnell@gmail.com>
2025-03-08 13:45:08 +0000

    .

--- backend/api/save-ancestor.js
+++ backend/api/save-ancestor.js
@@ -117,15 +117,14 @@ export default async function handler(req, res) {
       (rel) => rel + 1
     );
 
-    const parentGender =
-      ancestorDetails.sex === "male" ? "father_id" : "mother_id";
+    const parentGender = ancestorDetails.sex === "male" ? "father_id" : "mother_id";
 
     const { data: findChild, error: findChildError } = await supabase
       .from(`tree_${currentTree}`)
       .select("*")
       .eq(parentGender, ancestor_id);
 
-    console.log(findChild);
+    console.log("child is", findChild.first_name);
 
     //update child's presumed birth place if necessary. If there are generations of people with no birth place and no presumed birth place and the new ancestor has a birth place, this birthplace will be recursively assigned as the presumed birthplace of each descendant who lacks a birthplace
     const recursivelyUpdateChildsPresumedBirthPlace = async (
