39a9aa31d32e98fb7a103e4b7cc792c46d65db75
Cermait <markbodonnell@gmail.com>
2025-03-08 14:24:32 +0000

    .

--- backend/api/save-profile-info.js
+++ backend/api/save-profile-info.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     return;
   }
   try {
-    console.log("API triggered")
+    console.log("API triggered");
     const { userId, profileData } = req.body;
 
     // Get current tree id from users table
@@ -42,30 +42,30 @@ export default async function handler(req, res) {
 
     const currentTree = user.current_tree_id;
 
-    console.log("beginning to save info...")
-    console.log("alternative names:", profileData.alternative_names)
+    console.log("beginning to save info...");
+    console.log("alternative names:", profileData.alternative_names);
 
-    const {data, error}  = await supabase 
-        .from(`tree_${currentTree}`)
-        .update({
-            first_name: profileData.first_name,
-            middle_name: profileData.middle_name,
-            last_name: profileData.last_name,
-            place_of_birth: profileData.place_of_birth,
-            date_of_birth: profileData.date_of_birth,
-            place_of_death: profileData.place_of_death,
-            date_of_death: profileData.date_of_death,
-            cause_of_death: profileData.cause_of_death,
-            ethnicity: profileData.ethnicity,
-            alternative_names: profileData.alternative_names,
-            occupation: profileData.occupation,
-            paternal_haplogroup:  profileData.paternal_haplogroup,
-            maternal_haplogroup: profileData.maternal_haplogroup,
-            profile_pic_caption: profileData.ancestor_id
-        })
+    const { data, error } = await supabase.from(`tree_${currentTree}`).update({
+      first_name: profileData.first_name,
+      middle_name: profileData.middle_name,
+      last_name: profileData.last_name,
+      place_of_birth: profileData.place_of_birth,
+      date_of_birth: profileData.date_of_birth,
+      place_of_death: profileData.place_of_death,
+      date_of_death: profileData.date_of_death,
+      cause_of_death: profileData.cause_of_death,
+      ethnicity: profileData.ethnicity,
+      alternative_names: profileData.alternative_names,
+      occupation: profileData.occupation,
+      paternal_haplogroup: profileData.paternal_haplogroup,
+      maternal_haplogroup: profileData.maternal_haplogroup,
+      profile_pic_caption: profileData.ancestor_id,
+    })
+    .eq("ancestor_id", profileData.ancestor_id);
 
-        console.log("info saved")
 
+
+    console.log("info saved");
   } catch (error) {
     console.log("error saving profile info:", error);
   }
