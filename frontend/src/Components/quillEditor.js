adbb5633025f257d2f940ec479c2fc22cfc5e63b
Cermait <markbodonnell@gmail.com>
2025-03-09 01:57:54 +0000

    .

--- frontend/src/Components/quillEditor.js
+++ frontend/src/Components/quillEditor.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-// Custom keyboard handler to insert a linebreak without creating <p> tag
+// Custom keyboard handler to insert line breaks without creating <p> tag
 const modules = {
   toolbar: [
     [{ header: [1, 2, false] }],
@@ -16,15 +16,12 @@ const modules = {
       // Custom handler for the Enter key
       linebreak: {
         key: 13, // Enter key
-        handler: function (range) {
+        handler: function (range, context) {
           const quill = this.quill;
-          const cursorPosition = range.index;
-
-          // Insert line break <br> instead of a new paragraph <p>
-          quill.insertEmbed(cursorPosition, "break", true); // Insert a <br> tag
-
-          // Move the cursor to the next position
-          quill.setSelection(cursorPosition + 1);
+          
+          // Prevent the default Enter behavior (creating a new <p> tag)
+          quill.insertText(range.index, "\n");
+          quill.setSelection(range.index + 1); // Move cursor after inserted newline
         },
       },
     },
