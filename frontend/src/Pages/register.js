ecead0befaa64d871e17c14ba4bd97267527347f
Cermait <markbodonnell@gmail.com>
2025-03-12 22:21:08 +0000

    .

--- frontend/src/Pages/register.js
+++ frontend/src/Pages/register.js
@@ -14,17 +14,18 @@ const Register = () => {
     setError(''); 
 
     if (password !== confirmedPassword) {
-      alert(`Passwords do not match: ${password} - ${confirmedPassword}`);
+      alert(`Passwords do not match`);
       return;
     }
 ;
     try {
 
-
       // Handle successful registration (e.g., store token and redirect)
       localStorage.setItem('token', response.data.token); // Save token in localStorage
       localStorage.setItem('username', response.data.user.username);
       localStorage.setItem('userId', response.data.user.id);
+
+      console.log(username)
 
       const response = await axios.post('https://cleirigh-backend.vercel.app/api/register', {
         username,
