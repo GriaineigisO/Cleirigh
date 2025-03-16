6596ee9f9546a8a76800d77bc099ba148aa08679
Cermait <markbodonnell@gmail.com>
2025-03-16 23:52:47 +0000

    .

--- frontend/src/Pages/migrationMap.js
+++ frontend/src/Pages/migrationMap.js
@@ -56,30 +56,6 @@ const FamilyMigrationMap = () => {
 
     infoControl.addTo(initMap);
 
-    const streets = L.tileLayer(
-      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
-      {
-        attribution:
-          "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012",
-      }
-    );
-  
-    const satellite = L.tileLayer(
-      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-      {
-        attribution:
-          "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-      }
-    );
-
-  
-    const basemaps = {
-      Streets: streets,
-      Satellite: satellite,
-    };
-  
-    // Add layer control to the map
-    L.control.layers(basemaps).addTo(initMap);
 
     setMap(initMap);
   }, []);
