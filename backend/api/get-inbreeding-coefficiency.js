0523b655b0fa84001790d1497d4f1da0ed179d76
Cermait <markbodonnell@gmail.com>
2025-04-11 04:25:27 +0100

    .

--- backend/api/get-inbreeding-coefficiency.js
+++ backend/api/get-inbreeding-coefficiency.js
@@ -131,8 +131,6 @@ export default async function handler(req, res) {
           // Apply a scaling factor to reduce impact of high n values
           commonCoEff += Math.pow(0.5, n) * (1 + F_CA);
 
-          // Adding the common ancestor's contribution to the inbreeding coefficient
-          commonCoEff += Math.pow(0.5, n) * (1 + F_CA); // Formula for inbreeding coefficient contribution
         }
       }
 
